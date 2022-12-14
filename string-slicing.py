3f88af728f1dc14543bf206985fccf663ccd4ca6
Rahul Kumar Gupta <rahulshivan05@gmail.com>
2022-12-14 16:46:14 +0530

    String slicing in Python

---
+++ string-slicing.py
@@ -0,0 +1,35 @@
+# names = "Rahul, Rohit"
+
+# find the length of names
+# print(len(names))
+
+# print(names[0:5])
+
+# starts with x end with (x-1)
+
+fruit = "Mango"
+mangoLen = len(fruit)
+# print(mangoLen)
+# print(fruit[0:4])
+# print(fruit[:4]) # python automatically put 0 at begginig
+# print(fruit[1:4]) # including 1 but not include 4
+# print(fruit[0:5]) # including 0 but not include 5
+# print(fruit[0:-3])
+
+# Python actually do like this above equation solve
+# print(fruit[0:len(fruit)-3])
+
+# solve this equation like:
+# -1 => 4
+# -3 => 2
+# 4:2
+# including 4 but not include 2
+# print(fruit[-1:len(fruit)-3]) # it doesn't print anything because -1 doesn't exists
+
+# solve equation like:
+# -3 => 2
+# -1 => 4
+# 2:4
+# including 2 but not include 4
+# print(fruit[-3:-1])
+
