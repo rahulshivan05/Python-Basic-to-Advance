4028a54e488f9f21a7e03b063da3ece2d40171be
Rahul Kumar Gupta <rahulshivan05@gmail.com>
2022-12-14 15:58:28 +0530

    Input fuction in Python

---
+++ input.py
@@ -0,0 +1,8 @@
+# a = input("Enter your name: ")
+# print("My name is:",a)
+
+
+# Python input() takes as a string
+x = input("Enter first number: ")
+y = input("Enter first number: ")
+print(int(x)+int(y))
