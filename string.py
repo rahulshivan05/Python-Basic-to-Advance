bd36c42ac0c055135e8356ce4e1e30dee1d23520
Rahul Kumar Gupta <rahulshivan05@gmail.com>
2022-12-14 16:21:45 +0530

    String in Python

---
+++ string.py
@@ -0,0 +1,29 @@
+name = "Rahul"
+friend = 'Rohit'
+apple = "He said \"I want to eat an apple"
+# print("Hello", friend)
+
+# another way to do
+apple = 'He said "I want to eat an apple'
+# print(apple)
+
+# Multi-line String
+
+passage = '''My name is Rahul Kumar Gupta
+897. And I want to learn Python after C programmig Language.
+So thanks, for watching my Github Account. and I" wana want to eat an Apple.'''
+
+# print(passage)
+
+''' In python string is likely an array of character '''
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+# print(name[5]) # Throw an error because not print anything because Rahul length is 5.
+
+for character in passage:
+    print(character)
+
+
