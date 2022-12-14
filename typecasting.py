a048c23d8538a9016b4f5144ddfd3b465f866211
Rahul Kumar Gupta <rahulshivan05@gmail.com>
2022-12-14 15:38:22 +0530

    first commit

---
+++ typecasting.py
@@ -0,0 +1,28 @@
+# Explective Type-casting
+
+'''
+jab ham kisi data-type ko another data-type convert karte hai
+'''
+
+a = '1'
+# a = '1wah' # gives the error when put 
+b = '2'
+# print(int(a)+int(b))
+
+# Implicitive Type-casting
+
+'''
+Jo python khud karta hai usa kahata hai Implicitive type casting
+'''
+
+c = 1.9
+# print(type(c))
+
+d = 8
+# print(type(d))
+
+e = c+d
+# print(type(e))
+
+print(e)
+
---
+++ variable-dataType.py
@@ -0,0 +1,32 @@
+a = 1165165463
+# print(a)
+# print(type(a))
+
+# Strings always written in ("") double quotes
+b = "Rahul" 
+# print(b)
+# print(type(b))
+
+c = None
+# print(type(c))
+
+d = complex(8,2)
+# print(type(d))
+
+# Sequence data type
+
+list1 = [8,2.3,[-4,5], ["apple", "banana"]]
+print(list1)
+
+''' Tuple is Imutable '''
+tuple1 = (("parrot", "sparrow", "Lion", "Tiger"))
+print(tuple1)
+
+# Dictionary is map data. Make map with another thing
+
+dict1 = {"name": "Rahul", "age": 20, "canVote": True}
+print(dict1)
+print(type(dict1))
+
+# Python me every thing is object.
+# if you print(type(dict1)) then you got <class 'dict'> means object hai class dict ka
