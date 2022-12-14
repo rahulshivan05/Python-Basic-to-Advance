81e303e67585f3dbcaa7d02eccc9babdf0e3f248
Rahul Kumar Gupta <rahulshivan05@gmail.com>
2022-12-14 15:47:23 +0530

    arithmetic operations in Python

---
+++ arithmatic.py
@@ -0,0 +1,17 @@
+print(5+6)
+print(15-6)
+print(15*6)
+print(15/6) # ans is (2.5)
+
+# Flor division operator
+# only show (.) before value like above
+print(15//6) # it show's only (2)
+
+# Modulas
+# it show's only remainder
+
+print(5%3) 
+
+# Exponential Opertor
+
+print(5**3) # it resolves like (5*5*5 => 125)
