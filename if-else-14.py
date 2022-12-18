d9274d899abada4306811a833968618be283757c
Rahul Kumar Gupta <rahulshivan05@gmail.com>
2022-12-18 17:00:05 +0530

    if else in Python

---
+++ if-else-14.py
@@ -0,0 +1,17 @@
+# a = int(input("Enter your age: "))
+
+# print("Your age is:", a)
+
+# if(a>18):
+#     print("you can drive")
+# else:
+#     print("You cann't drive")
+
+num = float(input("Enter the number: "))
+
+if(num < 0):
+    print("Number is Negative")
+elif(num == 0):
+    print("Number is Zero")
+else:
+    print("Number is positive")
