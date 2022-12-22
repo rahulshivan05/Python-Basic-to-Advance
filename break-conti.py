8e05376d7157e44ef568dafe792be2b5ed3035e2
Rahul Kumar Gupta <rahulshivan05@gmail.com>
2022-12-22 16:06:11 +0530

    Break and Continue in Python

---
+++ break-conti.py
@@ -0,0 +1,21 @@
+for i in range(11):
+    # print("5 x", i+0, "=", i*5)
+
+    # Another way of doing
+    # if (i==10):
+    #     break
+
+    # Continue
+    # if (i==10):
+    #     continue
+
+    # print("5 X", i+1, "=", 5*(i+1))
+    pass
+
+i = 0
+while True:
+    print(i)
+    i = i+1
+    if(i%100 == 0):
+        break
+
