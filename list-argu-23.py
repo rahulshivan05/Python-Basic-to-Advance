f2c45cb66d6958375d3d59162c2531e63905bf88
Rahul Kumar Gupta <rahulshivan05@gmail.com>
2022-12-22 17:27:30 +0530

    List argument in Python

---
+++ list-argu-23.py
@@ -0,0 +1,34 @@
+# list are Muttable
+
+l = [11, 45,1,2,4,6, 1]
+# print(l)
+# print(type(l))
+
+# l.append(7)
+# l.sort(reverse=True)
+# sort method sorted the list
+
+# l.reverse()
+
+# print(l.index(1)) # 1 occurs is 2 time
+
+# print(l.count(1))
+
+# m = l
+# m = l.copy()
+# m[0] = 0 
+# because m is a refrence by l
+# That cause l has changed when m is changed
+
+# l.insert(1, 899)
+
+# extend means open l and extend m value in l
+m = [900,1000,1100]
+k = l+m
+print(k)
+
+
+
+print(l)
+
+# print("M is:",m)
