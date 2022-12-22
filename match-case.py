62c2c1977db997adb91d7ae067cda39b19c7e75e
Rahul Kumar Gupta <rahulshivan05@gmail.com>
2022-12-22 15:25:10 +0530

    Match Case in Python 3.11

---
+++ match-case.py
@@ -0,0 +1,21 @@
+# import os
+
+# print("Hello World from ...")
+# os.system("python --version")
+
+x = int(input("Enter the value of x: "))
+match x:
+    case 0:
+        print("x is zero")
+        # break automatic hota hai
+    case 4:
+        print("case is 4")
+    
+    case _ if x!= 90:
+        print(x, "is not 90")
+    
+    case _ if x!= 80:
+        print(x, "is not 80")
+
+    case _:
+        print(x)
