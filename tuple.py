9cbd9c41db8a12f9da051f2b30fcc9e525a54b4d
Rahul Kumar Gupta <rahulshivan05@gmail.com>
2022-12-22 17:04:48 +0530

    Tuples in Python

---
+++ tuple.py
@@ -0,0 +1,17 @@
+# Tuples are Im-muttable
+
+tup = (1,2, 76, 342, 55, "Rahul", "Green", True)
+
+# print(type(tup), tup)
+print(len(tup))
+print(tup[0])
+print(tup[-1])
+# print(tup[2])
+# print(tup[3])
+# print(tup[4])
+# print(tup[5])
+# print(tup[6])
+print(tup[::])
+
+# if 342 in tup:
+#     print("Yes 342 is present in the tuple")
