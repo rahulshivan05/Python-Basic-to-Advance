aa99bb6e4700edecc9c26e62f3b824a6cb12a42f
Rahul Kumar Gupta <rahulshivan05@gmail.com>
2022-12-22 15:41:05 +0530

    For Loop in Python

---
+++ for-loop.py
@@ -0,0 +1,28 @@
+# name = 'Abhishek'
+
+# for i in name:
+#     print(i)
+#     if i=="b":
+#         print("This is something special!")
+
+# 2nd example
+
+# colors = ['Red', 'Green', 'Blue', 'Yellow']
+
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+# starts with 0 and,
+# goes to (x-1) means (5-1 => 4)
+
+# for k in range(1, 20001):
+#     print(k)
+
+# for k in range(1, 12, 2): # starts with 1 and goes to 12 and jump in 2
+#     print(k)
+
+# for k in range(1,12,3):
+#     print(k)
+
