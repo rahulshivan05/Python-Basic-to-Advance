c5d7389dcd487cc3b932227ee939f2cb97679b65
Rahul Kumar Gupta <rahulshivan05@gmail.com>
2022-12-22 16:21:46 +0530

    Function in Python

---
+++ function.py
@@ -0,0 +1,20 @@
+def calculateGmean(a, b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def isGreater(a,b):
+    if(a>b):
+        print("First Number is Greater")
+    else:
+        print("Second Number is Greater")
+
+def isLesser(a,b):
+    pass # means i will do it later
+
+
+a = 9
+b = 8
+# gmean = (a*b)/(a+b)
+# print(gmean)
+calculateGmean(a,b)
+isGreater(a, b)
